f3e62e7f17c60999e2fd4f4e94d1bdc50f5a0eab
Qone2 <whrbdnjs33@naver.com>
2022-11-12 11:30:52 +0900

    ✨ feat: Address 엔티티 설정

---
+++ server/src/main/java/com/ilchinjo/mainproject/Address/entity/Address.java
@@ -0,0 +1,26 @@
+package com.ilchinjo.mainproject.Address.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    private String sido;
+
+    private String sigungu;
+
+    private String bname;
+}
