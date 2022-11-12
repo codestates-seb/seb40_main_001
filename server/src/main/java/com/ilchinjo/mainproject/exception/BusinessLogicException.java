48c1734d9207c484bcaad873fb5c049466e62483
Qone2 <whrbdnjs33@naver.com>
2022-11-12 10:54:28 +0900

    ✨ feat: BusinessLogicException 생성

---
+++ server/src/main/java/com/ilchinjo/mainproject/exception/BusinessLogicException.java
@@ -0,0 +1,13 @@
+package com.ilchinjo.mainproject.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessLogicException extends RuntimeException {
+    private final ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
