601c13b4e5673b4616b4bba63970ab35d327645b
Qone2 <whrbdnjs33@naver.com>
2022-11-12 11:00:12 +0900

    ✨ feat: 공통 MultiResponseDto 추가

---
+++ server/src/main/java/com/ilchinjo/mainproject/dto/PageInfo.java
@@ -0,0 +1,18 @@
+package com.ilchinjo.mainproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+@Getter
+@AllArgsConstructor
+public class PageInfo {
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+
+    public static <T> PageInfo of(Page<T> page) {
+        return new PageInfo(page.getNumber() + 1, page.getSize(), page.getTotalElements(), page.getTotalPages());
+    }
+}
