fdce9a6e9df5041fab8672866b8b1094773f7ec8
Qone2 <whrbdnjs33@naver.com>
2022-11-12 10:31:22 +0900

    🎉 init: be 프로젝트 첫 커밋

---
+++ server/src/main/java/com/ilchinjo/mainproject/MainprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ilchinjo.mainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainprojectApplication.class, args);
+	}
+
+}
