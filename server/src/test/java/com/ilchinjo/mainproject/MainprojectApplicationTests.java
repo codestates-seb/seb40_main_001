d961743e6fb6a72fa4a0f0746b6bcdeb9a63e383
Gyuwon Cho <whrbdnjs33@naver.com>
2022-11-14 21:17:38 +0900

    ✨ feat: 백엔드 초기 세팅
    * 🎉 init: be 프로젝트 첫 커밋
    
    * ✨ feat: ExceptionCode 생성
    
    * ✨ feat: BusinessLogicException 생성
    
    * ✨ feat: 전역 ExceptionAdvice 추가
    
    * ✨ feat: Auditing 추가
    
    * ✨ feat: 공통 MultiResponseDto 추가
    
    * ✨ feat: application.yml 설정
    
    * ✨ feat: cors를 위한 webconfig 설정
    
    나중에 SecurityConfig이 추가되면 Configuration 어노테이션을 비활성화 합니다.
    
    * ✨ feat: Address 엔티티 설정
    
    * ✨ feat: Member 엔티티 설정
    
    * ♻️ refactor: 프로젝트 구조 변경
    
    domain, global 구분
    
    * ♻️ refactor: member 엔티티 조정
    
    * ✨ feat: 운동친구모집 엔티티 설정
    
    * ✨ feat: 댓글 엔티티 설정
    
    * :bug: fix: 누락된 연관관계 추가 및 잘못된 이름 수정
    
    * :sparkles: feat: 대댓글 엔티티 설정
    
    * :fire: remove: 빠지게된 컬럼 삭제
    
    * :sparkles: feat: 리뷰 엔티티 작성
    
    * :sparkles: feat: 신청 엔티티 작성
    
    * :recycle: refactor: 누락된 Auditing 엔티티 설정

---
+++ server/src/test/java/com/ilchinjo/mainproject/MainprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilchinjo.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
