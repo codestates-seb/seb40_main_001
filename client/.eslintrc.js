978a7f6bad4c5e32bf7adb9e0312ae5cb02a4852
haileyport <sugar9935@gmail.com>
2022-11-14 22:15:32 +0900

    ✨ feat: 프론트엔드 초기 세팅

---
+++ client/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 14,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  /* eslint-disable global-require */
+  plugins: [require("daisyui")],
+};
