b5292583c993feded5e4727fad337ee82f328d4d
hyeongiii <hyeongiii0811@gmail.com>
2022-11-15 01:01:10 +0900

    :sparkles: feat: ExerciseService 인터페이스 생성

---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/service/ExerciseService.java
@@ -0,0 +1,8 @@
+package com.ilchinjo.mainproject.domain.exercise.service;
+
+import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
+
+public interface ExerciseService {
+
+    Exercise saveExercise(Exercise exercise);
+}
