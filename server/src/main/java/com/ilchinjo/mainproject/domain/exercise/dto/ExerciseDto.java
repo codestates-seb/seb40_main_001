b34216326e456409a62c29be6459282f84c3691f
hyeongiii <hyeongiii0811@gmail.com>
2022-11-15 17:20:06 +0900

    :bug: fix: 제약조건에 대한 validator를 찾을 수 없는 오류 해결 javax.validation.UnexpectedTypeException String이 아닌 타입의 필드에 @NotBlank 어노테이션을 붙여 발생한 오류로, @NotNull로 바꿔줌으로써 해결하였다.

--- server/src/main/java/com/ilchinjo/mainproject/domain/exercise/dto/ExerciseDto.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/dto/ExerciseDto.java
@@ -3,14 +3,19 @@ package com.ilchinjo.mainproject.domain.exercise.dto;
 import com.ilchinjo.mainproject.domain.address.entity.Address;
 import com.ilchinjo.mainproject.domain.exercise.entity.Category;
 import com.ilchinjo.mainproject.domain.exercise.entity.GenderType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Getter
 public class ExerciseDto {
 
+    @Getter
+    @Builder
+    @AllArgsConstructor
     public static class Post {
         @NotBlank(message = "제목은 공백이 아니어야 합니다.")
         private String title;
@@ -18,16 +23,16 @@ public class ExerciseDto {
         @NotBlank(message = "본문은 공백이 아니어야 합니다.")
         private String content;
 
-        @NotBlank(message = "운동 시작시점는 공백이 아니어야 합니다.")
+        @NotNull(message = "운동 시작시점는 공백이 아니어야 합니다.")
         private LocalDateTime exerciseAt;
 
-        @NotBlank(message = "운동 종료시점은 공백이 아니어야 합니다.")
+        @NotNull(message = "운동 종료시점은 공백이 아니어야 합니다.")
         private LocalDateTime endAt;
 
-        @NotBlank(message = "성별 타입은 공백이 아니어야 합니다.")
+        @NotNull(message = "성별 타입은 공백이 아니어야 합니다.")
         private GenderType genderType;
 
-        @NotBlank(message = "카테고리는 공백이 아니어야 합니다.")
+        @NotNull(message = "카테고리는 공백이 아니어야 합니다.")
         private Category category;
     }
 }
