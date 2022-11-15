0ac0f5334cea16bdb66de51b20e3a69957a1a47a
hyeongiii <hyeongiii0811@gmail.com>
2022-11-15 13:43:22 +0900

    :sparkles: feat: ExerciseServiceImpl 생성

---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/service/ExerciseServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ilchinjo.mainproject.domain.exercise.service;
+
+import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
+import com.ilchinjo.mainproject.domain.exercise.repository.ExerciseRepository;
+import com.ilchinjo.mainproject.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExerciseServiceImpl implements ExerciseService {
+
+    private final ExerciseRepository exerciseRepository;
+
+    @Override
+    public Exercise saveExercise(Exercise exercise) {
+
+        return exerciseRepository.save(exercise);
+    }
+}
