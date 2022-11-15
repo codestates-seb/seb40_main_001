6055356913b8275acaafd8e0914822898efc5fec
yujung-Jang <yjung.dev@gmail.com>
2022-11-16 01:17:27 +0900

    :sparkles: feat: Member 엔티티의 DTO 구현

---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberResponseDto.java
@@ -0,0 +1,11 @@
+package com.ilchinjo.mainproject.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberResponseDto {
+
+    private String memberId;
+}
--- server/src/main/java/com/ilchinjo/mainproject/domain/member/mapper/MemberMapper.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/mapper/MemberMapper.java
@@ -1,12 +1,14 @@
 package com.ilchinjo.mainproject.domain.member.mapper;
 
-import com.ilchinjo.mainproject.domain.member.dto.MemberDto;
+import com.ilchinjo.mainproject.domain.member.dto.MemberPostDto;
+import com.ilchinjo.mainproject.domain.member.dto.MemberResponseDto;
 import com.ilchinjo.mainproject.domain.member.entity.Member;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
 
-    Member postDtoToEntity(MemberDto.Post postDto);
-    MemberDto.Response entityToResponseDto(Member member);
+    Member postDtoToEntity(MemberPostDto postDto);
+
+    MemberResponseDto entityToResponseDto(Member member);
 }
