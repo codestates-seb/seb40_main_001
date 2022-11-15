ee53c451f774e81e4c89dbbb1b79d0c552eb2c96
yujung-Jang <yjung.dev@gmail.com>
2022-11-15 17:15:37 +0900

    :sparkles: feat: MemberService에 회원 가입 추상 메서드 구현

---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.ilchinjo.mainproject.domain.member.service;
+
+import com.ilchinjo.mainproject.domain.member.entity.Member;
+
+public interface MemberService {
+
+    Member saveMember(Member member);
+}
