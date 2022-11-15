29ed7ae55d28c0dea92ee6587a5aaae2f352a2d8
nada1221 <nada931221@gmail.com>
2022-11-15 23:09:15 +0900

    ✨feat: Add HomeCardLayouts

---
+++ client/src/components/HomeCardLayouts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeCL = () => {
+  return (
+    <div className="card w-[350px] h-[150px] bg-border_color card-bordered border-2 border-main drop-shadow-lg"></div>
+  );
+};
+
+export default HomeCL;
