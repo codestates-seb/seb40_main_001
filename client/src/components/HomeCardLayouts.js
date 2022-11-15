c98fd0857e01c35b7f99c45e37b3924239ead884
nada1221 <nada931221@gmail.com>
2022-11-15 23:12:40 +0900

    🩹chore: Add rounded-[20px]

--- client/src/components/HomeCardLayouts.js
+++ client/src/components/HomeCardLayouts.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomeCL = () => {
   return (
-    <div className="card w-[350px] h-[150px] bg-border_color card-bordered border-2 border-main drop-shadow-lg"></div>
+    <div className="card w-[350px] h-[150px] bg-border_color card-bordered border-2 border-main drop-shadow-lg rounded-[20px]"></div>
   );
 };
 
