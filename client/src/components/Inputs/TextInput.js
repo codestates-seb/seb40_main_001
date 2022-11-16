34c2fcd3b41dad746dcdacd2421661485f337071
nada1221 <nada931221@gmail.com>
2022-11-16 14:48:01 +0900

    ✨feat:Add molecules Text Input

--- client/src/components/Inputs/TextInput.js
+++ client/src/components/Inputs/TextInput.js
@@ -3,7 +3,7 @@ import { React } from 'react';
 const TextInputArea = ({ target }) => {
   return (
     <textarea
-      className="textarea border-main focus:outline-none rounded-[7px] w-[267px] h=[90px]"
+      className="textarea bg-gray border-main focus:outline-none rounded-[7px] w-[267px] h=[90px]"
       placeholder={`${target}을 입력해주세요`}
     ></textarea>
   );
