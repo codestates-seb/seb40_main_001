e2c7110524190756b3b621f226c578bc0580742f
haileyport <sugar9935@gmail.com>
2022-11-16 16:18:06 +0900

    ♻️refactor : 성별 선택 버튼 재사용성 추가
    성별 선택 버튼과 필터 버튼을 함께 사용할 수 있게 리팩토링

--- client/src/components/UI/atoms/ChooseBtn.js
+++ client/src/components/UI/atoms/ChooseBtn.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 
-const ChooseBtn = () => {
+const ChooseBtn = ({ contents, size }) => {
   const [isSelect, setIsSelect] = useState(false);
-  const gender = ['남성', '여성'];
 
   const handleClick = idx => {
-    const newArr = Array(gender.length).fill(false);
+    const newArr = Array(contents.length).fill(false);
     newArr[idx] = true;
     setIsSelect(newArr);
   };
-
+  // 333*60, 129*30
+  const wd = size === 'small' ? 'w-[129px] h-[30px]' : 'w-[333px] h-[60px]';
   return (
-    <div className="w-[333px] h-[60px] rounded-[7px] border-2 border-main">
-      {gender.map((ele, idx) => {
+    <div className={`${wd} rounded-[7px] border-2 border-main`}>
+      {contents.map((ele, idx) => {
         return idx === 0 ? (
           <button
             key={idx}
             className={
               isSelect[idx]
-                ? 'w-3/6 h-full rounded-[5px] rounded-r-none bg-main text-white text-400'
-                : 'w-3/6 h-full rounded-[5px] rounded-r-none bg-white text text-400'
+                ? 'w-3/6 h-full rounded-[5px] rounded-r-none bg-main text-white text-400 border-r border-main'
+                : 'w-3/6 h-full rounded-[5px] rounded-r-none bg-white text text-400 border-r border-main'
             }
             onClick={() => handleClick(idx)}
           >
