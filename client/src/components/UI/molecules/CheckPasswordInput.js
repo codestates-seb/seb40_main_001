f4c109401ce907911f71a34077ea65437d5706f4
nada1221 <nada931221@gmail.com>
2022-11-16 21:14:21 +0900

    ♻️refactor: div tag -> label tag

--- client/src/components/UI/molecules/CheckPasswordInput.js
+++ client/src/components/UI/molecules/CheckPasswordInput.js
@@ -2,13 +2,12 @@ import { React } from 'react';
 import CheckingPassword from '../atoms/CheckingPassword';
 
 const CheckPasswordInput = () => {
-  const target = '비밀번호 확인';
   return (
     <div className="display.flex">
       <label htmlFor="PasswordCheck" className="text text-[15px] mb-[5px]">
-        {'비밀번호 확인'}
+        비밀번호 확인
       </label>
-      <CheckingPassword target={target} />
+      <CheckingPassword />
     </div>
   );
 };
