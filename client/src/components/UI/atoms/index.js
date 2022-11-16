e78b59f58c8a44ef8413739d27fc602ea4c0105d
haileyport <sugar9935@gmail.com>
2022-11-16 16:51:59 +0900

    ✨feat : atom에 index 추가

---
+++ client/src/components/UI/atoms/index.js
@@ -0,0 +1,20 @@
+// Buttons
+import ChooseBtn from './ChooseBtn';
+import CommentBtn from './CommentBtn';
+import EditBtn from './EditBtn';
+import ExerciseBtn from './ExerciseBtn';
+import LightBtn from './LightBtn';
+import LongBtn from './LongBtn';
+import MiniBtn from './MiniBtn';
+import ShortBtn from './ShortBtn';
+
+export {
+  ChooseBtn,
+  CommentBtn,
+  EditBtn,
+  ExerciseBtn,
+  LightBtn,
+  LongBtn,
+  MiniBtn,
+  ShortBtn,
+};
