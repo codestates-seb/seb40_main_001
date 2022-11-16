a889918a994fbe80baa95f31eb9b2b027afc9a4c
nada1221 <nada931221@gmail.com>
2022-11-16 22:22:23 +0900

    🩹chore: placeholder change

--- client/src/components/UI/atoms/WriteContentsInput.js
+++ client/src/components/UI/atoms/WriteContentsInput.js
@@ -1,10 +1,10 @@
 import { React } from 'react';
 
-const ContentsInput = ({ target }) => {
+const ContentsInput = () => {
   return (
     <textarea
       type="text"
-      placeholder={`${target} 입력해주세요`}
+      placeholder="내용을 입력해주세요"
       className="resize-none bg-none pt-[30px] w-[350px] h-[300px] border-main border-b-[1px] focus:outline-none text-300
       "
     />
