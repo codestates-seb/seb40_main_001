64bda3b5042605bad781c1ec3d878d63194fb526
haileyport <sugar9935@gmail.com>
2022-11-17 21:41:07 +0900

    ✨feat : molecules index 생성

---
+++ client/src/components/UI/molecules/index.js
@@ -0,0 +1,43 @@
+import ArounderReview from './ArounderReview';
+import Carousel from './Carousel';
+import CheckPasswordinput from './CheckPasswordInput';
+import ViewrCLX from './CommentViewer_X';
+import ViewrCL from './CommentViewer';
+import Dropdown from './Dropdown';
+import ExerciseCarousel from './ExerciseCarousel';
+import ExerciseCategory from './ExerciseCategory';
+import HistoryCL from './HistoryCards';
+import IdInput from './IdInput';
+import InputComments from './InputComments';
+import MiniDropdown from './MiniDropdown';
+import Nickname from './Nickname';
+import NicknameMypage from './NicknameMypage';
+import PasswordInput from './PasswordInput';
+import ReviewMent from './ReviewMent';
+import WriteHNC from './WriteHNC';
+import WriterCLX from './WriterCardLayouts_X';
+import WriterCL from './WriterCardLayouts';
+import WriterDetail from './WriterDetail';
+
+export {
+  ArounderReview,
+  Carousel,
+  CheckPasswordinput,
+  ViewrCLX,
+  ViewrCL,
+  Dropdown,
+  ExerciseCarousel,
+  ExerciseCategory,
+  HistoryCL,
+  IdInput,
+  InputComments,
+  MiniDropdown,
+  Nickname,
+  NicknameMypage,
+  PasswordInput,
+  ReviewMent,
+  WriteHNC,
+  WriterCLX,
+  WriterCL,
+  WriterDetail,
+};
