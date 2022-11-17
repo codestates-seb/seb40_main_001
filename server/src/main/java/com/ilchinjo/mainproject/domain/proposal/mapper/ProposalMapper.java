2d7e655f65b7013cc47908975e7b84e0338f6d7a
Qone2 <whrbdnjs33@naver.com>
2022-11-17 15:23:37 +0900

    :sparkles: feat: 신청리스트 조회 서비스계층 구현

--- server/src/main/java/com/ilchinjo/mainproject/domain/proposal/mapper/ProposalMapper.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/proposal/mapper/ProposalMapper.java
@@ -1,10 +1,15 @@
 package com.ilchinjo.mainproject.domain.proposal.mapper;
 
 import com.ilchinjo.mainproject.domain.proposal.dto.ProposalResponseDto;
+import com.ilchinjo.mainproject.domain.proposal.dto.ProposalSimpleResponseDto;
 import com.ilchinjo.mainproject.domain.proposal.entity.Proposal;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProposalMapper {
     ProposalResponseDto entityToResponseDto(Proposal proposal);
+
+    List<ProposalSimpleResponseDto> entitiesToSimpleResponseDtoList(List<Proposal> proposals);
 }
