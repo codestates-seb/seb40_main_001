02bb5c35641c6ea619fd758bca8fe3bd99be8deb
hyeongiii <hyeongiii0811@gmail.com>
2022-11-17 22:32:04 +0900

    :sparkles: feat: Controller 운동모집 전체조회 핸들러 메서드 구현

--- server/src/main/java/com/ilchinjo/mainproject/domain/exercise/controller/ExerciseController.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/controller/ExerciseController.java
@@ -1,13 +1,10 @@
 package com.ilchinjo.mainproject.domain.exercise.controller;
 
-import com.ilchinjo.mainproject.domain.exercise.dto.ExerciseDetailResponseDto;
-import com.ilchinjo.mainproject.domain.exercise.dto.ExercisePatchDto;
-import com.ilchinjo.mainproject.domain.exercise.dto.ExercisePostDto;
-import com.ilchinjo.mainproject.domain.exercise.dto.ExerciseResponseDto;
+import com.ilchinjo.mainproject.domain.exercise.dto.*;
 import com.ilchinjo.mainproject.domain.exercise.service.ExerciseService;
+import com.ilchinjo.mainproject.global.dto.MultiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -40,6 +37,16 @@ public class ExerciseController {
     public ExerciseDetailResponseDto getExercise(@PathVariable(name = "exercise-id") Long exerciseId) {
 
         return exerciseService.findExercise(exerciseId);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public MultiResponseDto<ExerciseResponseDto> getExercises(@RequestHeader(name = "Authorization") Long memberId,
+                                                              @RequestParam String address,
+                                                              @RequestParam String category,
+                                                              @RequestParam(name = "gender-type") String genderType) {
+
+        return MultiResponseDto.of(exerciseService.findExercises(address, genderType, category, memberId));
     }
 
     @DeleteMapping("/{exercise-id}")
