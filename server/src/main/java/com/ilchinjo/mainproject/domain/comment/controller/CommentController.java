256d4186dbcf67fcb5543ded0673a265120b7bae
yujung-Jang <yjung.dev@gmail.com>
2022-11-17 10:51:36 +0900

    :sparkles: feat: 댓글 작성 핸들러 메서드 구현

---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/comment/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.ilchinjo.mainproject.domain.comment.controller;
+
+import com.ilchinjo.mainproject.domain.comment.dto.CommentPostDto;
+import com.ilchinjo.mainproject.domain.comment.dto.CommentResponseDto;
+import com.ilchinjo.mainproject.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/exercises")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{exercise-id}/comment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentResponseDto postComment(@PathVariable("exercise-id") Long exerciseId,
+                                          @RequestHeader("Authorization") Long memberId,
+                                          @RequestBody CommentPostDto postDto) {
+
+        return commentService.saveComment(exerciseId, memberId, postDto);
+    }
+}
