deeecb74148d42477a2d1bc2c20710c81d90fb3f
hyeongiii <hyeongiii0811@gmail.com>
2022-11-17 15:28:51 +0900

    :sparkles: feat: ReviewServiceImpl 생성

--- server/src/main/java/com/ilchinjo/mainproject/domain/review/repository/ReviewRepository.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.ilchinjo.mainproject.domain.review.repository;
 
+import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
 import com.ilchinjo.mainproject.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+     List<Review> findByExercise(Exercise exercise);
 }
---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/review/service/ReviewServiceImpl.java
@@ -0,0 +1,69 @@
+package com.ilchinjo.mainproject.domain.review.service;
+
+import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
+import com.ilchinjo.mainproject.domain.exercise.service.ExerciseService;
+import com.ilchinjo.mainproject.domain.member.entity.Member;
+import com.ilchinjo.mainproject.domain.member.service.MemberService;
+import com.ilchinjo.mainproject.domain.review.dto.ReviewPostDto;
+import com.ilchinjo.mainproject.domain.review.dto.ReviewResponseDto;
+import com.ilchinjo.mainproject.domain.review.entity.Review;
+import com.ilchinjo.mainproject.domain.review.mapper.ReviewMapper;
+import com.ilchinjo.mainproject.domain.review.repository.ReviewRepository;
+import com.ilchinjo.mainproject.global.exception.BusinessLogicException;
+import com.ilchinjo.mainproject.global.exception.ExceptionCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReviewServiceImpl implements ReviewService{
+
+    private final ReviewMapper reviewMapper;
+    private final ReviewRepository reviewRepository;
+    private final MemberService memberService;
+    private final ExerciseService exerciseService;
+
+    @Override
+    public ReviewResponseDto saveReview(ReviewPostDto postDto, Long memberId, Long exerciseId) {
+
+        Member findSrcMember = memberService.findVerifiedMember(memberId);
+        Member findDestMember = memberService.findVerifiedMember(postDto.getDestMember().getMemberId());
+        Exercise findExercise = exerciseService.findVerifiedExercise(exerciseId);
+        // 권한 확인
+        checkAuthorization(findExercise, findSrcMember, findDestMember);
+        // 리뷰 중복 체크
+        checkDuplicatedReview(findExercise, findSrcMember);
+
+        Review review = reviewMapper.postDtoToEntity(postDto);
+        Review createdReview = Review.createReview(review, findSrcMember, findDestMember, findExercise);
+        reviewRepository.save(createdReview);
+
+        return reviewMapper.entityToResponseDto(createdReview);
+    }
+
+    private void checkAuthorization(Exercise exercise, Member srcMember, Member destMember) {
+        if ((srcMember.equals(exercise.getHost()) && destMember.equals(exercise.getParticipant()))
+                || (srcMember.equals(exercise.getParticipant()) && destMember.equals(exercise.getHost())))
+            return;
+
+        throw new BusinessLogicException(ExceptionCode.MEMBER_UNAUTHORIZED);
+    }
+
+    private void checkDuplicatedReview(Exercise exercise, Member srcMember) {
+
+        List<Review> reviewList = reviewRepository.findByExercise(exercise);
+        if (reviewList.isEmpty()) return;
+        if (reviewList.size() == 1) {
+            ArrayList<Review> reviewArrayList = (ArrayList<Review>) reviewList;
+            Member findSrcMember = reviewArrayList.get(0).getSrcMember();
+            if (!srcMember.equals(findSrcMember)) return;
+        }
+
+        throw new BusinessLogicException(ExceptionCode.REVIEW_EXISTS);
+    }
+}
