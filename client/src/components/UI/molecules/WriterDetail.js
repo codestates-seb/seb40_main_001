95a6f6a22040cc24eef3956dbab8181a2e0f6592
nada1221 <nada931221@gmail.com>
2022-11-17 11:50:09 +0900

    🩹chore : code change

--- client/src/components/UI/molecules/WriterDetail.js
+++ client/src/components/UI/molecules/WriterDetail.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import HomeCard from '../atoms/HomeCardImg';
 
-const WriterDetail = ({ startDate, endDate }) => {
+const WriterDetail = ({ title, nickname, startDate, endDate }) => {
+  const target = 'img src route';
   return (
     <div className="flex flex-row justify-between w-[350px] h-[70px] border-b-[1px] border-main">
       <div className="flex flex-col">
-        <div className="text-400 mb-[5px]">같이 배드민턴 치실 분!</div>
+        <div className="text-400 mb-[5px]">{`${title}`}</div>
         <div className="flex flex-row items-center">
-          <HomeCard />
-          <div className="text-200">자몽스무디</div>
+          <HomeCard target={target} />
+          <div className="text-200">{`${nickname}`}</div>
         </div>
       </div>
       <div className="flex text-low text-200  items-end">{`${startDate} ~ ${endDate}`}</div>
