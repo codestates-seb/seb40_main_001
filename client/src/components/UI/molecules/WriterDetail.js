3792d92ef12fc38a3e87e7b379a1ae41351b969e
nada1221 <nada931221@gmail.com>
2022-11-17 11:51:13 +0900

    🩹chore: code change const -> props

--- client/src/components/UI/molecules/WriterDetail.js
+++ client/src/components/UI/molecules/WriterDetail.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import HomeCard from '../atoms/HomeCardImg';
 
-const WriterDetail = ({ title, nickname, startDate, endDate }) => {
-  const target = 'img src route';
+const WriterDetail = ({ title, nickname, startDate, endDate, target }) => {
   return (
     <div className="flex flex-row justify-between w-[350px] h-[70px] border-b-[1px] border-main">
       <div className="flex flex-col">
