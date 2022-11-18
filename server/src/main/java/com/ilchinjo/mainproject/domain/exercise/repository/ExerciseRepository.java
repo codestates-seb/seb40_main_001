5b1432e0f23f3da5eee0028849a00fc269882c04
hyeongiii <hyeongiii0811@gmail.com>
2022-11-18 13:13:25 +0900

    :adhesive_bandage: chore: ExerciseRepository 구현할 인터페이스 추가

--- server/src/main/java/com/ilchinjo/mainproject/domain/exercise/repository/ExerciseRepository.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/repository/ExerciseRepository.java
@@ -3,5 +3,5 @@ package com.ilchinjo.mainproject.domain.exercise.repository;
 import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+public interface ExerciseRepository extends JpaRepository<Exercise, Long>, ExerciseRepositoryCustom {
 }
