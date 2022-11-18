cb330000941cefca75d4f15069d595880b7127c7
nada1221 <nada931221@gmail.com>
2022-11-18 11:43:58 +0900

    ✨feat: Add Header Arrow.ver

---
+++ client/src/components/UI/molecules/HeaderArrow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ReactComponent as LeftArrow } from '../../../assets/img/icons/leftArrow.svg';
+
+const HeaderArrow = ({ txt }) => {
+  const arrowHandler = () => {
+    console.log('arrow');
+  };
+
+  return (
+    <div className="flex flex-row justify-between w-[390px] h-[55px] items-center">
+      <LeftArrow onClick={arrowHandler} />
+      <div className="text-400">{txt}</div>
+      <div className="w-[40px]"></div>
+    </div>
+  );
+};
+
+export default HeaderArrow;
