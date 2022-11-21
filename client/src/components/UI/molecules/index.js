023410c08b795adf0769d68fdb402ab4b2d9dc48
nada1221 <nada931221@gmail.com>
2022-11-21 17:37:08 +0900

    🩹chore: Add index code

--- client/src/components/UI/molecules/index.js
+++ client/src/components/UI/molecules/index.js
@@ -1,11 +1,14 @@
 import ArounderReview from './ArounderReview';
+import ApplicantSet from './Applicant';
 import Carousel from './Carousel';
 import CheckPasswordinput from './CheckPasswordInput';
-import ViewrCLX from './CommentViewer_X';
-import ViewrCL from './CommentViewer';
 import Dropdown from './Dropdown';
 import ExerciseCarousel from './ExerciseCarousel';
 import ExerciseCategory from './ExerciseCategory';
+import GenderSelect from './GenderSelect';
+import HeaderArrow from './HeaderArrow';
+import HeaderFind from './HeaderFind';
+import HeaderLogo from './HeaderLogo';
 import HistoryCL from './HistoryCards';
 import IdInput from './IdInput';
 import InputComments from './InputComments';
@@ -14,12 +17,18 @@ import Nickname from './Nickname';
 import NicknameMypage from './NicknameMypage';
 import PasswordInput from './PasswordInput';
 import ReviewMent from './ReviewMent';
+import TownSelect from './TownSelect';
+import ViewrCLX from './CommentViewer_X';
+import ViewrCL from './CommentViewer';
+import WriteDate from './WriteDate';
+import WriteGenderSelect from './WriteGenderSelect';
 import WriteHNC from './WriteHNC';
 import WriterCLX from './WriterCardLayouts_X';
 import WriterCL from './WriterCardLayouts';
 import WriterDetail from './WriterDetail';
 
 export {
+  ApplicantSet,
   ArounderReview,
   Carousel,
   CheckPasswordinput,
@@ -40,4 +49,11 @@ export {
   WriterCLX,
   WriterCL,
   WriterDetail,
+  GenderSelect,
+  HeaderArrow,
+  HeaderFind,
+  HeaderLogo,
+  TownSelect,
+  WriteDate,
+  WriteGenderSelect,
 };
