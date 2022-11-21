305e48ca9c15fc7b47bdce324cf4113ee3ba7100
Seunghwan <ysp1307@naver.com>
2022-11-21 23:49:22 +0900

    ✨ feat: HeaderNone 구현

--- client/src/components/UI/molecules/index.js
+++ client/src/components/UI/molecules/index.js
@@ -22,6 +22,7 @@ import WriteHNC from './WriteHNC';
 import WriterCLX from './WriterCardLayouts_X';
 import WriterCL from './WriterCardLayouts';
 import WriterDetail from './WriterDetail';
+import HeaderNone from './HeaderNone';
 
 export {
   ArounderReview,
@@ -48,4 +49,5 @@ export {
   WriterCLX,
   WriterCL,
   WriterDetail,
+  HeaderNone,
 };
