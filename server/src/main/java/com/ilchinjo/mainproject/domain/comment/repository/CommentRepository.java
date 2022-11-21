93750353e98bb2641a575b3cb7332fa89c330f6f
yujung-Jang <yjung.dev@gmail.com>
2022-11-21 14:14:24 +0900

    :recycle: refactor: Comment 데이터 액세스 수정

--- server/src/main/java/com/ilchinjo/mainproject/domain/comment/repository/CommentRepository.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/comment/repository/CommentRepository.java
@@ -9,9 +9,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByExerciseOrderByCommentId(Exercise exercise, Pageable page);
-
-    List<Comment> findAllByExerciseAndCommentIdGreaterThanOrderByCommentId(Exercise exercise, Long id, Pageable page);
+    List<Comment> findAllByExercise(Exercise exercise, Pageable page);
 
     Boolean existsByExerciseAndCommentIdGreaterThan(Exercise exercise, Long id);
 }
