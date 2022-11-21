448aa4e8eb3c6bb5d0b61c35e063d8051f09a0f5
Qone2 <whrbdnjs33@naver.com>
2022-11-21 22:10:58 +0900

    :adhesive_bandage: chore: 신청 응답 dto에 상태값 포함

--- server/src/main/java/com/ilchinjo/mainproject/domain/proposal/dto/ProposalResponseDto.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/proposal/dto/ProposalResponseDto.java
@@ -2,6 +2,7 @@ package com.ilchinjo.mainproject.domain.proposal.dto;
 
 import com.ilchinjo.mainproject.domain.exercise.dto.ExerciseResponseDto;
 import com.ilchinjo.mainproject.domain.member.dto.MemberResponseDto;
+import com.ilchinjo.mainproject.domain.proposal.entity.ProposalStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,4 +16,5 @@ public class ProposalResponseDto {
     private Long proposalId;
     private ExerciseResponseDto exercise;
     private MemberResponseDto participant;
+    private ProposalStatus proposalStatus;
 }
