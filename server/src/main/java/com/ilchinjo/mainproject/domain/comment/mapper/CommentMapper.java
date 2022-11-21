2beb0ad93295ef0347eeedd6ad1bdb020a528591
yujung-Jang <yjung.dev@gmail.com>
2022-11-21 10:48:03 +0900

    :sparkles: feat: entitiesToResponseDtoList 추가

--- server/src/main/java/com/ilchinjo/mainproject/domain/comment/mapper/CommentMapper.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/comment/mapper/CommentMapper.java
@@ -5,10 +5,14 @@ import com.ilchinjo.mainproject.domain.comment.dto.CommentResponseDto;
 import com.ilchinjo.mainproject.domain.comment.entity.Comment;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
 
     Comment postDtoToEntity(CommentPostDto postDto);
 
     CommentResponseDto entityToResponseDto(Comment comment);
+
+    List<CommentResponseDto> entitiesToResponseDtoList(List<Comment> comment);
 }
