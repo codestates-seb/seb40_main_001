fb7857373a4d04ad3366543705bdfbca80670749
yujung-Jang <yjung.dev@gmail.com>
2022-11-21 10:48:42 +0900

    :sparkles: feat: Comment 데이터 액세스 계층 메서드 추가

--- server/src/main/java/com/ilchinjo/mainproject/domain/comment/repository/CommentRepository.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,17 @@
 package com.ilchinjo.mainproject.domain.comment.repository;
 
 import com.ilchinjo.mainproject.domain.comment.entity.Comment;
+import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findAllByExerciseOrderByCommentId(Exercise exercise, Pageable page);
+
+    List<Comment> findAllByExerciseAndCommentIdGreaterThanOrderByCommentId(Exercise exercise, Long id, Pageable page);
+
+    Boolean existsByExerciseAndCommentIdGreaterThan(Exercise exercise, Long id);
 }
