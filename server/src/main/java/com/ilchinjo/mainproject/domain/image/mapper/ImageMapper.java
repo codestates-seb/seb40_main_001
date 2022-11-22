b3f22381f02f4564a6799823481f1591895a85c8
hyeongiii <hyeongiii0811@gmail.com>
2022-11-23 00:40:09 +0900

    :sparkles: feat: ImageMapper 메서드 추가

--- server/src/main/java/com/ilchinjo/mainproject/domain/image/mapper/ImageMapper.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/image/mapper/ImageMapper.java
@@ -4,8 +4,12 @@ import com.ilchinjo.mainproject.domain.image.dto.ImageResponseDto;
 import com.ilchinjo.mainproject.domain.image.entity.Image;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ImageMapper {
 
     ImageResponseDto entityToResponseDto(Image image);
+
+    List<ImageResponseDto> entitiesToResponseDtos(List<Image> images);
 }
