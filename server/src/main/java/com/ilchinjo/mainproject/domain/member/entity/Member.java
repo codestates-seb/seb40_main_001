306d29cf0c9244b8df5582e5a7cb153728b4eb0d
hyeongiii <hyeongiii0811@gmail.com>
2022-11-22 21:27:10 +0900

    :adhesive_bandage: chore: Member 엔티티에 Image 필드 추가

--- server/src/main/java/com/ilchinjo/mainproject/domain/member/entity/Member.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/entity/Member.java
@@ -3,6 +3,7 @@ package com.ilchinjo.mainproject.domain.member.entity;
 import com.ilchinjo.mainproject.domain.address.entity.Address;
 import com.ilchinjo.mainproject.domain.comment.entity.Comment;
 import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
+import com.ilchinjo.mainproject.domain.image.entity.Image;
 import com.ilchinjo.mainproject.domain.reply.entity.Reply;
 import com.ilchinjo.mainproject.domain.review.entity.Review;
 import com.ilchinjo.mainproject.global.audit.AuditingEntity;
@@ -59,6 +60,9 @@ public class Member extends AuditingEntity {
 
     @Builder.Default
     private int publicEvaluation = 20;
+
+    @OneToOne(mappedBy = "profiledMember", cascade = CascadeType.REMOVE)
+    private Image image;
 
     public void update(Member member, Address address) {
 
