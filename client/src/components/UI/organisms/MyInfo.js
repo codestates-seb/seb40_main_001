3faa6024976e2927964714542c00e6ebe8217104
haileyport <sugar9935@gmail.com>
2022-11-22 15:17:16 +0900

    🐛fix: MyInfo width 수정

--- client/src/components/UI/organisms/MyInfo.js
+++ client/src/components/UI/organisms/MyInfo.js
@@ -5,7 +5,7 @@ import HNM from '../atoms/HNM';
 
 const MyInfo = ({ profile, nickname, charge }) => {
   return (
-    <div className="flex flex-row h-[90px] w-[380px] pt-0.5 border-b border-b-main">
+    <div className="flex flex-row h-[90px] w-[350px] pt-0.5 border-b border-b-main">
       <HNM target={profile} />
       <div className="flex flex-col ml-[15px]">
         <div className="mb-[10px]">
