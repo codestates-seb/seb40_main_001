a19886988bd29ea1c1e79b602677cc92f360f07e
nada1221 <nada931221@gmail.com>
2022-11-22 20:05:54 +0900

    ✨feat: Add Landing Page

---
+++ client/src/components/templates/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Around } from '../../assets/img/icons/around.svg';
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-row justify-center items-center h-screen">
+      <Around />
+    </div>
+  );
+};
+
+export default LandingPage;
