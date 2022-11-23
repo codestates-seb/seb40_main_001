b93c6fec8216c419b1bab2733b297f1c7d173fcd
haileyport <sugar9935@gmail.com>
2022-11-23 21:52:39 +0900

    ✨feat: organisms index 추가

---
+++ client/src/components/UI/organisms/index.js
@@ -0,0 +1,19 @@
+import ArounderRecord from './ArounderRecord';
+import Comments from './Comments';
+import DetailContents from './DetailContents';
+import Drawer from './Drawer';
+import HomeContents from './HomeContents';
+import Modal from './Modal';
+import MyInfo from './MyInfo';
+import WriteContents from './WriteContents';
+
+export {
+  ArounderRecord,
+  Comments,
+  DetailContents,
+  Drawer,
+  HomeContents,
+  Modal,
+  MyInfo,
+  WriteContents,
+};
