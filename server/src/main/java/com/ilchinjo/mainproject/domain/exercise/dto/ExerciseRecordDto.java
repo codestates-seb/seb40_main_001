df725a884cb8323545a869ca3b3bd4cc405efbb1
Qone2 <whrbdnjs33@naver.com>
2022-11-23 15:32:11 +0900

    :sparkles: feat: 운동기록용 dto생성

---
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/dto/ExerciseRecordDto.java
@@ -0,0 +1,32 @@
+package com.ilchinjo.mainproject.domain.exercise.dto;
+
+import com.ilchinjo.mainproject.domain.address.entity.Address;
+import com.ilchinjo.mainproject.domain.exercise.entity.Category;
+import com.ilchinjo.mainproject.domain.exercise.entity.ExerciseStatus;
+import com.ilchinjo.mainproject.domain.exercise.entity.GenderType;
+import com.ilchinjo.mainproject.domain.member.dto.MemberResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseRecordDto {
+    private Long exerciseId;
+    private MemberResponseDto host;
+    private MemberResponseDto participant;
+    private String title;
+    private String content;
+    private LocalDateTime exerciseAt;
+    private LocalDateTime endAt;
+    private GenderType genderType;
+    private Category category;
+    private ExerciseStatus exerciseStatus;
+    private Address address;
+    private Boolean isReviewed;
+}
