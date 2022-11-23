e167ed15a595b6448b7f3d712e8f0a989fb5cb89
Qone2 <whrbdnjs33@naver.com>
2022-11-23 13:58:59 +0900

    :bug: fix: 응답 dto 잘못된 타입 수정

--- server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberDetailResponseDto.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberDetailResponseDto.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 public class MemberDetailResponseDto {
 
     private Long memberId;
-    private Long email;
+    private String email;
     private String nickname;
     private int publicEvaluation;
     // TODO 운동 친구 기록
