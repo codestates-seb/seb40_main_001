604b8af8dc97ea08aedb959d03613b4071424a6e
yujung-Jang <yjung.dev@gmail.com>
2022-11-23 14:27:06 +0900

    :sparkles: feat: 로그인 인증 실패 시의 추가 로직 구현

---
+++ server/src/main/java/com/ilchinjo/mainproject/global/security/handler/MemberAuthenticationFailureHandler.java
@@ -0,0 +1,51 @@
+package com.ilchinjo.mainproject.global.security.handler;
+
+import com.google.gson.Gson;
+import com.ilchinjo.mainproject.global.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+
+        log.error("# Authentication failed: {}", exception.getMessage());
+
+        String errorMessage;
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
+        } else if (exception instanceof InternalAuthenticationServiceException) {
+            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "계정이 존재하지 않습니다. 회원가입 후 로그인 해주세요.";
+        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
+            errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
+        } else {
+            errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다 관리자에게 문의하세요.";
+        }
+
+        sendErrorResponse(response, errorMessage);
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, String errorMessage) throws IOException {
+
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED, errorMessage);
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(401);
+        response.getWriter().write(new Gson().toJson(errorResponse));
+    }
+}
