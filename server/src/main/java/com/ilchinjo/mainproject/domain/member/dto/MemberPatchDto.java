60a6bb7aebbd250532dbab1be296067d579790c3
hyeongiii <hyeongiii0811@gmail.com>
2022-11-24 13:11:42 +0900

    :adhesive_bandage: chore: MemberPatchDto validation 수정

--- server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberPatchDto.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberPatchDto.java
@@ -5,15 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberPatchDto {
 
-    @NotBlank
     private String nickname;
 
     private Long addressId;
