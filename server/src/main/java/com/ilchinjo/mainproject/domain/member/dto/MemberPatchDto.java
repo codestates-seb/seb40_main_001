5475df311149c9da47982ebe4af442507fe0a070
hyeongiii <hyeongiii0811@gmail.com>
2022-11-24 13:53:08 +0900

    :adhesive_bandage: chore: MemberPatchDto에 custom 어노테이션 적용

--- server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberPatchDto.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/member/dto/MemberPatchDto.java
@@ -1,19 +1,27 @@
 package com.ilchinjo.mainproject.domain.member.dto;
 
+import com.ilchinjo.mainproject.global.validator.notspacelong.NotSpaceLong;
+import com.ilchinjo.mainproject.global.validator.notspacestring.NotSpaceString;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@NotBlank
 public class MemberPatchDto {
 
+    @NotSpaceString
     private String nickname;
 
+    @NotSpaceLong
     private Long addressId;
 
+    @NotSpaceLong
     private Long imageId;
 }
