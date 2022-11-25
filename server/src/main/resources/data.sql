b098fa63450e13862b1b9fbab493f01c6605b3d6
Qone2 <whrbdnjs33@naver.com>
2022-11-25 14:25:20 +0900

    :adhesive_bandage: chore: 중복 시 행동 설정
    제약조건 오류가 나면 프로그램 실행이 안돼서 넣었습니다.
    insert ignore into ~~ 도 가능합니다.

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -23,7 +23,8 @@ values (now(), now(), '서울특별시', '강남구'),
        (now(), now(), '서울특별시', '은평구'),
        (now(), now(), '서울특별시', '종로구'),
        (now(), now(), '서울특별시', '중구'),
-       (now(), now(), '서울특별시', '중랑구');
+       (now(), now(), '서울특별시', '중랑구')
+on duplicate key update created_at = created_at;
 
 
 
