7c7cdd5f0b13bff6a97367fcf52b4213a48746d8
Seunghwan Kim <ysp1307@naver.com>
2022-11-25 13:51:24 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-=======
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/pages/Landing';
 import LoginTemplate from './components/pages/LoginTemplate';
