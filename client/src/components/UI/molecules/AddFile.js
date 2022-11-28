a0063d26944f89fd480f13dfed1f935afa03d129
haileyport <sugar9935@gmail.com>
2022-11-28 17:14:08 +0900

    🐛bug: 추가된 이미지 렌더링 버그 수정

--- client/src/components/UI/molecules/AddFile.js
+++ client/src/components/UI/molecules/AddFile.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { FileDeleteBtn } from '../atoms';
 
-const AddFile = ({ img, handler, handleClick }) => {
+const AddFile = ({ img, handler, handleDelete }) => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row mb-[14px]">
         <div className="mr-[30px] text-300 text-low">첨부파일</div>
-        {img.map((el, idx) => {
-          return (
-            <div key={el} className="flex flex-row items-center mr-[5px]">
-              {el.name}
-              <FileDeleteBtn key={el} idx={idx} handler={handleClick} />
-            </div>
-          );
-        })}
+        {img &&
+          img.map((el, idx) => {
+            return (
+              <div key={el} className="flex flex-row items-center mr-[5px]">
+                {el.name}
+                <FileDeleteBtn key={el} idx={idx} handler={handleDelete} />
+              </div>
+            );
+          })}
       </div>
       <input
         type="file"
