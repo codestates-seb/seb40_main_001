767cf43d1ca547d29a27d20a18a4847809578a2e
Qone2 <whrbdnjs33@naver.com>
2022-11-28 16:13:10 +0900

    :bug: fix: proposalStatus가 null로 나오던 문제 수정
    https://www.baeldung.com/mapstruct#2-the-mapper-interface-1 이곳을 참조하였습니다.
    https://stackoverflow.com/questions/48521903/map-custom-method-mapper-to-mapstruct 여기에 소개된 방법으로 메핑할 메서드를 설정할 수도 있습니다.

--- server/src/main/java/com/ilchinjo/mainproject/domain/proposal/mapper/ProposalMapper.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/proposal/mapper/ProposalMapper.java
@@ -4,12 +4,15 @@ import com.ilchinjo.mainproject.domain.proposal.dto.ProposalResponseDto;
 import com.ilchinjo.mainproject.domain.proposal.dto.ProposalSimpleResponseDto;
 import com.ilchinjo.mainproject.domain.proposal.entity.Proposal;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProposalMapper {
+    @Mapping(target = "proposalStatus", source = "proposal.status")
     ProposalResponseDto entityToResponseDto(Proposal proposal);
 
+    @Mapping(target = "proposalStatus", source = "proposal.status")
     List<ProposalSimpleResponseDto> entitiesToSimpleResponseDtoList(List<Proposal> proposals);
 }
