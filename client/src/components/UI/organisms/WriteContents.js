cd877426dd2090f72f7c6b89c8bdf8a920dfc639
haileyport <sugar9935@gmail.com>
2022-11-28 17:14:40 +0900

    🩹chore: 글쓰기 컴포넌트 props 수정

--- client/src/components/UI/organisms/WriteContents.js
+++ client/src/components/UI/organisms/WriteContents.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   ExerciseCategory,
   WriteHNC,
@@ -7,36 +7,7 @@ import {
   AddFile,
 } from '../molecules';
 
-const WriteContents = () => {
-  const [data, setData] = useState({
-    exercise: '',
-    title: '',
-    content: '',
-    gender: '',
-    img: [],
-    startTime: new Date(),
-    endTime: new Date(),
-  });
-
-  const dataHandler = (target, change) => {
-    const newData = data;
-    newData[target] = change;
-    setData(newData);
-  };
-
-  const fileHandler = e => {
-    const newData = data;
-    newData.img = [...data.img, e.target.files[0]];
-    setData(newData);
-  };
-
-  const handleClick = idx => {
-    const newData = data;
-    const newArr = newData.img.filter((el, id) => id !== idx);
-    newData.img = newArr;
-    setData(newData);
-  };
-
+const WriteContents = ({ dataHandler, fileHandler, handleDelete, img }) => {
   return (
     <div className="flex flex-col w-[390px]">
       <ExerciseCategory handler={dataHandler} />
@@ -49,16 +20,16 @@ const WriteContents = () => {
         </div>
         <div className="flex flex-col py-3 border-b border-b-main">
           <div className="mb-[10px]">
-            <WriteDate start={true} handler={dataHandler} data={data} />
+            <WriteDate start={true} handler={dataHandler} />
           </div>
-          <WriteDate handler={dataHandler} data={data} />
+          <WriteDate handler={dataHandler} />
         </div>
         <div className="mt-[18px] mb-[27px]">
           {/* data.img를 가지고 첨부파일 리스트를 업데이트 하는데 조금 많이 느린 이슈 존재 */}
           <AddFile
-            img={data.img}
+            img={img}
             handler={fileHandler}
-            handleClick={handleClick}
+            handleDelete={handleDelete}
           />
         </div>
       </div>
