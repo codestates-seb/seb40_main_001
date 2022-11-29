8915aadd8655909398f082b0a64e2f5c9bbf8053
Seunghwan <ysp1307@naver.com>
2022-11-29 22:57:22 +0900

    #174 🩹 chore: 드롭다운 보여지는 데이터 변경

--- client/src/components/UI/molecules/DropdownCity.js
+++ client/src/components/UI/molecules/DropdownCity.js
@@ -4,6 +4,7 @@ import { client } from '../../../client/client';
 
 const DropdownCity = ({ setcityNum }) => {
   const [city, setCity] = useState([]);
+  const [viewCity, setViewCity] = useState('동네를 선택하세요.');
   const getCities = () => {
     client.get('/addresses').then(res => {
       setCity(res.data.data);
@@ -14,8 +15,9 @@ const DropdownCity = ({ setcityNum }) => {
     getCities();
   }, []);
 
-  const handleClick = (e, idx) => {
+  const handleClick = (e, idx, viewData) => {
     setcityNum(idx);
+    setViewCity(viewData);
   };
 
   return (
@@ -24,16 +26,18 @@ const DropdownCity = ({ setcityNum }) => {
         tabIndex={0}
         className="w-[333px] h-[60px] btn m-1 rounded-[7px] border-2 border-main bg-white text text-400 justify-between pl-[22px] pr-[13px] hover:bg-white hover:border-2 hover:border-main"
       >
-        {/* {city} */}
+        {viewCity}
         <DownArrow />
       </label>
       <ul
         tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-white text text-400 rounded-box w-[338px]"
-        // h-[200px] overflow-y-scroll 왜 안되는거지 찾아야할 것 같다..
+        className="block h-[120px] dropdown-content menu p-2 shadow bg-white text text-400 rounded-box w-[338px] overflow-y-scroll"
       >
         {city.map((data, idx) => (
-          <li key={idx} onClick={e => handleClick(e.target, data.addressId)}>
+          <li
+            key={idx}
+            onClick={e => handleClick(e.target, data.addressId, data.sigungu)}
+          >
             <a className="active:bg-main active:text-white">{data.sigungu}</a>
           </li>
         ))}
