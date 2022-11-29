14068ac22c91f790e7d114c13d624a5930a3c8e9
haileyport <sugar9935@gmail.com>
2022-11-29 20:44:45 +0900

    ✨feat: 글쓰기 페이지 구현

--- client/src/components/pages/Write.js
+++ client/src/components/pages/Write.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LongBtn, HeaderArrow, WriteContents } from '../UI';
 import { client, clientImg } from '../../client/client';
@@ -13,8 +13,14 @@ const Write = () => {
     startTime: new Date(),
     endTime: new Date(),
   });
-  const [isDisabled, setIsDisabled] = useState(true);
   const [imgList, setImgList] = useState([]);
+  const [disableObj, setDisableObj] = useState({
+    exercise: true,
+    title: true,
+    content: true,
+    gender: true,
+  });
+  const [isDisable, setIsDisable] = useState(true);
 
   const navigate = useNavigate();
 
@@ -24,9 +30,20 @@ const Write = () => {
   };
 
   const dataHandler = (target, change) => {
-    const newData = data;
-    newData[target] = change;
-    setData(newData);
+    setData(prev => {
+      const newData = prev;
+      newData[target] = change;
+      return newData;
+    });
+    setDisableObj(prev => {
+      const newData = prev;
+      newData[target] = change.length <= 0;
+      return newData;
+    });
+
+    const val = Object.values(disableObj);
+    const res = val.filter(el => el === true).length;
+    setIsDisable(res > 0);
   };
 
   const handleDelete = idx => {
@@ -35,6 +52,10 @@ const Write = () => {
   };
 
   const submitWrite = () => {
+    if (!data.exercise || !data.title || !data.content || !data.gender) {
+      return;
+    }
+
     // 이미지 생성
     const imgUp = new FormData();
     imgList.forEach(el => {
@@ -77,26 +98,6 @@ const Write = () => {
     // 제출
   };
 
-  // 여기가 동작을 안 해서 사람을 미치게 하는 중 ㅠㅠ
-  // dep가 의미가 없는 상태... data로 넣어도 key로 넣어도 문제
-  useEffect(() => {
-    console.log(data);
-    console.log('-----------------');
-    if (data.exercise && data.title && data.content && data.gender) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(false);
-    }
-  }, [data.exercise, data.title, data.content, data.gender]);
-
-  // localStorage 임시
-  useEffect(() => {
-    localStorage.setItem(
-      'accessToken',
-      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoidGVzdEB0ZS5zdCIsIm1lbWJlcklkIjoxLCJzdWIiOiJ0ZXN0QHRlLnN0IiwiaWF0IjoxNjY5NjE3MjY1LCJleHAiOjE2Njk2MzE2NjV9.4jCQTTCh06yyh_btH5TeDGqhK36cFiyPdywHSY6x7e9E_VFjcszUVVpDqIBfJqppJalIvRFa0GTd_8N1o1DAWg',
-    );
-  }, []);
-
   return (
     <div>
       <HeaderArrow txt="글쓰기" />
@@ -108,7 +109,7 @@ const Write = () => {
         img={imgList}
       />
       <div className="flex w-full h-[30px] justify-center">
-        <LongBtn txt="글쓰기" onClick={submitWrite} disabled={isDisabled} />
+        <LongBtn txt="글쓰기" onClick={submitWrite} disabled={isDisable} />
       </div>
     </div>
   );
