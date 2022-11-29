2d0aeba1249d1f356ffe0f4448bbdd1fa4b05267
Seunghwan <ysp1307@naver.com>
2022-11-29 18:07:41 +0900

    ✨ feat: 데이터 임시 연결

--- client/src/components/UI/molecules/TownSelect.js
+++ client/src/components/UI/molecules/TownSelect.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DropdownCity from './DropdownCity';
 
 const TownSelect = ({ setcityNum }) => {
-  const [city, setCity] = useState('');
   return (
     <div className="flex flex-col text-left py-2">
       <label className="text font-bold text-200 mb-1">동네선택</label>
-      <DropdownCity city={city} setCity={setCity} setcityNum={setcityNum} />
+      <DropdownCity setcityNum={setcityNum} />
     </div>
   );
 };
