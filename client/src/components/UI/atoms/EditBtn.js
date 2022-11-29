68f5cde92b13e6d96474cc240f178cb2c4d6b43b
Seunghwan <ysp1307@naver.com>
2022-11-29 21:44:06 +0900

    #174💄ui: 위치 조정

--- client/src/components/UI/atoms/EditBtn.js
+++ client/src/components/UI/atoms/EditBtn.js
@@ -5,7 +5,7 @@ const EditBtn = ({ handleClick }) => {
   return (
     <button
       onClick={handleClick}
-      className="w-[55px] h-[55px] btn btn-circle drop-shadow bg-main-red hover:bg-main-red"
+      className="fixed bottom-12 right-10 w-[55px] h-[55px] btn btn-circle drop-shadow bg-main-red hover:bg-main-red"
     >
       <Edit />
     </button>
