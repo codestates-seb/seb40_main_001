6941b6f5c36460dc9e355d8d5c8f3ea4ea647772
Seunghwan <ysp1307@naver.com>
2022-11-29 23:55:13 +0900

    #174 ✨feat: 상세페이지 기능 임시 구현

--- client/src/components/UI/atoms/HomeCardLayouts.js
+++ client/src/components/UI/atoms/HomeCardLayouts.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const HomeCL = ({ source }) => {
+const HomeCL = ({ data, source, contentClick }) => {
+  const { exerciseId } = data || {};
   return (
-    <div className="card w-[350px] h-[150px] bg-white card-bordered border-2 border-main drop-shadow-lg rounded-[20px] hover:scale-105 transition duration-400">
+    <div
+      onClick={e => contentClick(e, exerciseId)}
+      className="card w-[350px] h-[150px] bg-white card-bordered border-2 border-main drop-shadow-lg rounded-[20px] hover:scale-105 transition duration-400"
+    >
       {source}
     </div>
   );
