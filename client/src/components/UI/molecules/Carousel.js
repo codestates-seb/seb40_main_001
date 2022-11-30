c7d069349bf09dc010c6377ef230f5fc4f88aded
nada1221 <nada931221@gmail.com>
2022-12-01 01:40:08 +0900

    #185 🩹chore : src -> remotePath code change

--- client/src/components/UI/molecules/Carousel.js
+++ client/src/components/UI/molecules/Carousel.js
@@ -9,15 +9,15 @@ const Carousel = ({ img }) => {
   return (
     <div className="carousel w-[368px] h-[231px]">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={img[0].src} className="w-full" />
+        <img src={img[0].remotePath} className="w-full" />
         <CarouselBtn href={['#slide3', '#slide2']} />
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src={img[1].src} className="w-full" />
+        <img src={img[1].remotePath} className="w-full" />
         <CarouselBtn href={['#slide1', '#slide3']} />
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src={img[2].src} className="w-full" />
+        <img src={img[2].remotePath} className="w-full" />
         <CarouselBtn href={['#slide2', '#slide1']} />
       </div>
     </div>
