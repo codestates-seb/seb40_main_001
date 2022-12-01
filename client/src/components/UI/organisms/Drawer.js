35b9ef06e04521a86c57e87b88dc612fb689f163
haileyport <sugar9935@gmail.com>
2022-12-01 12:06:10 +0900

    #183 🩹chore: 드로워 페이지 이동 추가

--- client/src/components/UI/organisms/Drawer.js
+++ client/src/components/UI/organisms/Drawer.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { DrawerInfo } from '../molecules';
 import { AddPeople, Mypage } from '../../../assets/img';
 
 const Drawer = ({ img, name }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-72 h-full p-[14px] bg-white">
       <DrawerInfo img={img} name={name} />
       <div className="border-2 border-gray mt-7"></div>
       <div className="ml-[14px] mt-4">
-        <div className="flex flex-row items-center text text-200 mb-[10px]">
+        <div
+          className="flex flex-row items-center text text-200 mb-[10px]"
+          onClick={() => navigate('/main')}
+        >
           <AddPeople />
           <div className="ml-[15px]">어라운더 찾기</div>
         </div>
-        <div className="flex flex-row items-center text text-200">
+        <div
+          className="flex flex-row items-center text text-200"
+          onClick={() => navigate('/mypage')}
+        >
           <Mypage />
           <div className="ml-[15px]">마이페이지</div>
         </div>
