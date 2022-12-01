7b2fc6af10b7b036f351e81d61a64aad8460f95a
haileyport <sugar9935@gmail.com>
2022-12-01 18:12:38 +0900

    #183 ✨feat: 이미지 수정 버튼 추가

--- client/src/components/UI/organisms/MyInfo.js
+++ client/src/components/UI/organisms/MyInfo.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { NicknameMypage } from '../molecules';
-import { BatteryCase } from '../atoms';
-import HNM from '../atoms/HNM';
+import { BatteryCase, HNM, ImgAddBtn } from '../atoms';
 
-const MyInfo = ({ userData }) => {
+const MyInfo = ({ userData, setUserData }) => {
   return (
     <div className="flex flex-row h-[90px] w-[350px] pt-0.5 border-b border-b-main">
-      <HNM target={userData && userData.profile} />
+      <div className="">
+        <HNM target={userData && userData.profile} />
+        <div className="fixed top-0 transform translate-x-8 translate-y-28">
+          <ImgAddBtn />
+        </div>
+      </div>
       <div className="flex flex-col ml-[15px]">
         <div className="mb-[10px]">
-          <NicknameMypage nickname={userData && userData.nickname} />
+          <NicknameMypage
+            setUserData={setUserData}
+            nickname={userData && userData.nickname}
+          />
         </div>
         <BatteryCase charge={userData && userData.charge} />
       </div>
