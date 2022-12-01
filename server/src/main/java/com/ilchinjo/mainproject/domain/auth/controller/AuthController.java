c98a08ea1d1fb03664d2ab861e30a10bdbf9edbc
yujung-Jang <yjung.dev@gmail.com>
2022-12-02 08:39:26 +0900

    #217 :sparkles: feat: refresh 핸들러 메서드 생성

--- server/src/main/java/com/ilchinjo/mainproject/domain/auth/controller/AuthController.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/auth/controller/AuthController.java
@@ -3,18 +3,27 @@ package com.ilchinjo.mainproject.domain.auth.controller;
 import com.ilchinjo.mainproject.domain.auth.service.AuthService;
 import com.ilchinjo.mainproject.global.security.jwt.JwtTokenizer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final JwtTokenizer jwtTokenizer;
     private final AuthService authService;
 
-    @PostMapping("/auth/logout")
+    @PostMapping("/refresh")
+    public void refresh(@CookieValue("Refresh") String refreshToken,
+                        HttpServletResponse response) {
+
+        String newAccessToken = authService.refreshToken(refreshToken);
+        response.setHeader("Authorization", "Bearer " + newAccessToken);
+    }
+
+    @PostMapping("/logout")
     public void logout(@RequestHeader("Authorization") String token) {
 
         Long memberId = jwtTokenizer.parseMemberId(token);
