42615a0e124e1ba881537e1cc9df87e43d2db31b
hyeongiii <hyeongiii0811@gmail.com>
2022-12-01 21:53:19 +0900

    #202 :adhesive_bandage: chore: 과거 구현한 파일 주석처리

--- server/src/main/java/com/ilchinjo/mainproject/domain/exercise/repository/ExerciseRepositoryCustomImpl.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/exercise/repository/ExerciseRepositoryCustomImpl.java
@@ -1,46 +1,45 @@
-package com.ilchinjo.mainproject.domain.exercise.repository;
-
-import com.ilchinjo.mainproject.domain.exercise.entity.Category;
-import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
-import com.ilchinjo.mainproject.domain.exercise.entity.GenderType;
-import com.ilchinjo.mainproject.domain.member.entity.Member;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import java.util.List;
-
-public class ExerciseRepositoryCustomImpl implements ExerciseRepositoryCustom {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    @Override
-    public List<Exercise> findExercises(String address, String genderType, String category, Member member) {
-
-        String jpql = "select e from Exercise e where ";
-
-        if (genderType.equals("ALL")) {
-            jpql += "(e.genderType = :all or e.host.gender = :gender) ";
-        } else if (genderType.equals("SAME")) {
-            jpql += "(e.host.gender = :gender) ";
-        }
-        if (!category.equals("ALL")) {
-            jpql += "and e.category = :category ";
-        }
-        jpql += "order by e.exerciseId desc ";
-        TypedQuery<Exercise> emQuery = em.createQuery(jpql, Exercise.class);
-
-        if (genderType.equals("ALL")) {
-            emQuery.setParameter("all", GenderType.ALL);
-            emQuery.setParameter("gender", member.getGender());
-        } else if (genderType.equals("SAME")) {
-            emQuery.setParameter("gender", member.getGender());
-        }
-        if (!category.equals("ALL")) {
-            emQuery.setParameter("category", Category.valueOf(category));
-        }
-
-        return emQuery.getResultList();
-    }
-}
+//package com.ilchinjo.mainproject.domain.exercise.repository;
+//
+//import com.ilchinjo.mainproject.domain.exercise.entity.Category;
+//import com.ilchinjo.mainproject.domain.exercise.entity.Exercise;
+//import com.ilchinjo.mainproject.domain.exercise.entity.GenderType;
+//import com.ilchinjo.mainproject.domain.member.entity.Member;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//import javax.persistence.TypedQuery;
+//import java.util.List;
+//
+//public class ExerciseRepositoryCustomImpl {
+//
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    public List<Exercise> findExercises(String address, String genderType, String category, Member member) {
+//
+//        String jpql = "select e from Exercise e where ";
+//
+//        if (genderType.equals("ALL")) {
+//            jpql += "(e.genderType = :all or e.host.gender = :gender) ";
+//        } else if (genderType.equals("SAME")) {
+//            jpql += "(e.host.gender = :gender) ";
+//        }
+//        if (!category.equals("ALL")) {
+//            jpql += "and e.category = :category ";
+//        }
+//        jpql += "order by e.exerciseId desc ";
+//        TypedQuery<Exercise> emQuery = em.createQuery(jpql, Exercise.class);
+//
+//        if (genderType.equals("ALL")) {
+//            emQuery.setParameter("all", GenderType.ALL);
+//            emQuery.setParameter("gender", member.getGender());
+//        } else if (genderType.equals("SAME")) {
+//            emQuery.setParameter("gender", member.getGender());
+//        }
+//        if (!category.equals("ALL")) {
+//            emQuery.setParameter("category", Category.valueOf(category));
+//        }
+//
+//        return emQuery.getResultList();
+//    }
+//}
