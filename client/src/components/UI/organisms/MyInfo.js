209884dbebfdec9a6eb667b699b5d5690730735d
haileyport <sugar9935@gmail.com>
2022-12-01 22:09:27 +0900

    ✨feat: 이미지수정 버튼 추가

--- client/src/components/UI/organisms/MyInfo.js
+++ client/src/components/UI/organisms/MyInfo.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { NicknameMypage } from '../molecules';
 import { BatteryCase, HNM, ImgAddBtn } from '../atoms';
 
-const MyInfo = ({ userData, setUserData }) => {
+const MyInfo = ({ userData, setUserData, imageHandler }) => {
   return (
     <div className="flex flex-row h-[90px] w-[350px] pt-0.5 border-b border-b-main">
       <div className="">
         <HNM target={userData && userData.profile} />
         <div className="fixed top-0 transform translate-x-8 translate-y-28">
-          <ImgAddBtn />
+          <ImgAddBtn handleFile={imageHandler} />
         </div>
       </div>
       <div className="flex flex-col ml-[15px]">
