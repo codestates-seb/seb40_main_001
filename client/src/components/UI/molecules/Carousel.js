b7462755224d6e46af74267589f2a92191e0682e
nada1221 <nada931221@gmail.com>
2022-12-01 15:32:53 +0900

    #185 🩹chore: img 길이 별 캐러셀 렌더 조정

--- client/src/components/UI/molecules/Carousel.js
+++ client/src/components/UI/molecules/Carousel.js
@@ -6,6 +6,33 @@ const Carousel = ({ img }) => {
   // 기존 = img src={img[0]}
   // 수정 = img src={img[0].src}
   // 데이터 받을 때 양식 맞춰서 바꾸면 될 것 같습니다.
+  if (img.length === 0) {
+    return <></>;
+  }
+  if (img.length === 1) {
+    return (
+      <div className="carousel w-[368px] h-[231px]">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img[0].remotePath} className="w-full" />
+          <CarouselBtn />
+        </div>
+      </div>
+    );
+  }
+  if (img.length === 2) {
+    return (
+      <div className="carousel w-[368px] h-[231px]">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img[0].remotePath} className="w-full" />
+          <CarouselBtn href={['#slide2']} />
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img[1].remotePath} className="w-full" />
+          <CarouselBtn href={['#slide1']} />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="carousel w-[368px] h-[231px]">
       <div id="slide1" className="carousel-item relative w-full">
