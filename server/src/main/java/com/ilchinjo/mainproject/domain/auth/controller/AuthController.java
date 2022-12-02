209057edf0f9c035227f10246e3125f35fe4237b
yujung-Jang <yjung.dev@gmail.com>
2022-12-02 11:20:36 +0900

    #217 :bug: bug: refresh 핸들러 메서드 로직 수정

--- server/src/main/java/com/ilchinjo/mainproject/domain/auth/controller/AuthController.java
+++ server/src/main/java/com/ilchinjo/mainproject/domain/auth/controller/AuthController.java
@@ -2,10 +2,13 @@ package com.ilchinjo.mainproject.domain.auth.controller;
 
 import com.ilchinjo.mainproject.domain.auth.service.AuthService;
 import com.ilchinjo.mainproject.global.security.jwt.JwtTokenizer;
+import com.ilchinjo.mainproject.global.security.utils.ErrorResponder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth")
@@ -17,10 +20,15 @@ public class AuthController {
 
     @PostMapping("/refresh")
     public void refresh(@CookieValue("Refresh") String refreshToken,
-                        HttpServletResponse response) {
+                        HttpServletResponse response) throws IOException {
 
         String newAccessToken = authService.refreshToken(refreshToken);
-        response.setHeader("Authorization", "Bearer " + newAccessToken);
+
+        if (newAccessToken != null) {
+            response.setHeader("Authorization", "Bearer " + newAccessToken);
+        } else {
+            ErrorResponder.sendExpiredJwtExceptionError(response, HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @PostMapping("/logout")
