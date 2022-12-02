92aeaac41891b3891d9814536b2ce4aca8c90679
Seunghwan <ysp1307@naver.com>
2022-12-02 23:12:18 +0900

    #211 🩹 chore: 홈으로 가기 추가 및 햄버거버튼

--- client/src/components/UI/molecules/HeaderLogo.js
+++ client/src/components/UI/molecules/HeaderLogo.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 // import { useNavigate } from 'react-router-dom';
 import { Logo, Menu } from '../../../assets/img';
 
 const HeaderLogo = ({ txt, menuHandler, children, menu = false }) => {
+  const navigate = useNavigate();
   const logoHandler = () => {
-    if (txt) {
-      // navigate('/');
-    }
+    navigate('/main');
   };
 
   return (
     <div className="flex flex-row justify-between w-[390px] h-[55px] items-center px-5">
-      <Logo onClick={logoHandler} />
+      <Logo onClick={logoHandler} className="cursor-pointer" />
       <div className="text-400">{txt}</div>
       <div className="absolute transform translate-x-[14.6rem]">{children}</div>
-      {menu ? <Menu onClick={menuHandler} /> : null}
+      {menu ? <Menu onClick={menuHandler} className="cursor-pointer" /> : null}
     </div>
   );
 };
