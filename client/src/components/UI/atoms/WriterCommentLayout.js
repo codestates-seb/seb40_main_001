2d5d983ba13e1317c04e6f61cad684fb3365a065
nada1221 <nada931221@gmail.com>
2022-12-03 00:17:37 +0900

    #235 🩹chore: 댓글 창 높이 제한 해제

--- client/src/components/UI/atoms/WriterCommentLayout.js
+++ client/src/components/UI/atoms/WriterCommentLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WriterCL = ({ source }) => {
   return (
-    <div className="card w-[319px] h-[70px] bg-babyPink card-bordered border-[1px] border-main-red rounded-[5px] drop-shadow-lg">
+    <div className="card w-[319px] bg-babyPink card-bordered border-[1px] border-main-red rounded-[5px] drop-shadow-lg">
       {source}
     </div>
   );
