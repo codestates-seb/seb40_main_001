4d2daee82ca49c493acb49299bd64254038bbdfe
Seunghwan <ysp1307@naver.com>
2022-12-04 02:35:46 +0900

    #271 ✨feat: 메타태그 추가

---
+++ client/src/components/meta/MetaTag.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const MetaTag = ({ title = 'Around' }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+
+      <meta name="description" content="주변 동네의 운동친구를 찾아보세요!" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={'Around'} />
+      <meta property="og:site_name" content={title} />
+      <meta
+        property="og:description"
+        content="주변 동네의 운동친구를 찾아보세요!"
+      />
+      <meta property="og:image" content={''} />
+      <meta property="og:url" content="https://aroundexercise.com/" />
+
+      <link rel="canonical" href="https://aroundexercise.com/" />
+      <link rel="icon" href="around.png" />
+    </Helmet>
+  );
+};
+
+export default MetaTag;
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import MetaTag from './components/meta/MetaTag';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
+      <MetaTag />
       <App />
     </HelmetProvider>
   </React.StrictMode>,
