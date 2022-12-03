2391b5bfa92783612e71d8fd9b2d77599919f01f
haileyport <sugar9935@gmail.com>
2022-12-03 16:28:01 +0900

    🐛bug: 이미지 수정 버튼 움직임 제거

--- client/src/components/UI/organisms/MyInfo.js
+++ client/src/components/UI/organisms/MyInfo.js
@@ -18,7 +18,7 @@ const MyInfo = ({ infoData, setUserData, imageHandler, changeName }) => {
     <div className="flex flex-row h-[90px] w-[350px] pt-0.5 border-b border-b-main">
       <div className="">
         <HNM target={profile} />
-        <div className="fixed top-0 transform translate-x-8 translate-y-28">
+        <div className="absolute top-0 transform translate-x-8 translate-y-28">
           <ImgAddBtn handleFile={imageHandler} />
         </div>
       </div>
