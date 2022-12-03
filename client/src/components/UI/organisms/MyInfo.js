999acf355154f875aa6a645f391a685fe95d022f
haileyport <sugar9935@gmail.com>
2022-12-03 15:41:52 +0900

    🐛bug: 이미지 렌더 지연 문제 해결

--- client/src/components/UI/organisms/MyInfo.js
+++ client/src/components/UI/organisms/MyInfo.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NicknameMypage } from '../molecules';
 import { BatteryCase, HNM, ImgAddBtn } from '../atoms';
 
-const MyInfo = ({ userData, setUserData, imageHandler, changeName }) => {
+const MyInfo = ({ infoData, setUserData, imageHandler, changeName }) => {
+  const [profile, setProfile] = useState();
+  const [name, setName] = useState();
+  const [charge, setCharge] = useState();
+
+  useEffect(() => {
+    if (infoData) {
+      setProfile(infoData.profile);
+      setName(infoData.nickname);
+      setCharge(infoData.charge);
+    }
+  }, [infoData]);
   return (
     <div className="flex flex-row h-[90px] w-[350px] pt-0.5 border-b border-b-main">
       <div className="">
-        <HNM target={userData && userData.profile} />
+        <HNM target={profile} />
         <div className="fixed top-0 transform translate-x-8 translate-y-28">
           <ImgAddBtn handleFile={imageHandler} />
         </div>
@@ -14,12 +25,12 @@ const MyInfo = ({ userData, setUserData, imageHandler, changeName }) => {
       <div className="flex flex-col ml-[15px]">
         <div className="mb-[10px]">
           <NicknameMypage
-            userData={userData}
+            nickname={name}
             setUserData={setUserData}
             changeName={changeName}
           />
         </div>
-        <BatteryCase charge={userData && userData.charge} />
+        <BatteryCase charge={charge} />
       </div>
     </div>
   );
